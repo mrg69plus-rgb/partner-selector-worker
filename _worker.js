f202a120347e54aac7ce0119257ecabbe8e6db90
mrg69plus-rgb <mrg69plus@gmail.com>
2026-09-08 23:22:45 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,31 +1,5 @@
 export default {
   async fetch(request, env) {
-    try {
-      const response = await fetch(
-        `https://api.telegram.org/bot${env.BOT_TOKEN}/getMe`
-      );
-
-      const result = await response.text();
-
-      return new Response(result, {
-        status: response.status,
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-
-    } catch (error) {
-      return new Response(
-        JSON.stringify({
-          error: error.message
-        }),
-        {
-          status: 500,
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-    }
+    return new Response("GITHUB VERSION 123");
   }
 };
